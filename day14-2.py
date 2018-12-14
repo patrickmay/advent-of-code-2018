cc5f6252c5eaba1fe4a87f6d50148d1263cee14d
Patrick May <patrick@softwarematters.org>
2018-12-14 10:59:11 -0500

    Day 2 solutions.

--- day14-2.py
+++ day14-2.py
@@ -9,23 +9,24 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         recipe_scores = sys.argv[1]
         recipes = [3,7]
-        recipes_string = '37'
         elf_indices = [0,1]
-        i = 0
-        index = -1
-        while True:
+        recipe_count = -1
+        while 1:
             new_recipe = 0
             for j in range(len(elf_indices)):
                 new_recipe += recipes[elf_indices[j]]
             for digit in str(new_recipe):
                 recipes.append(int(digit))
-                recipes_string += digit
+                if len(recipes) >= len(recipe_scores):
+                    recent_scores = ''.join([str(score)
+                                             for score
+                                             in recipes[-len(recipe_scores):]])
+                    if recent_scores == recipe_scores:
+                        recipe_count = len(recipes) - len(recipe_scores)
+                        print(recipe_count)
+                        sys.exit()
             for k in range(len(elf_indices)):
                 elf_indices[k] = ((elf_indices[k] + 1 + recipes[elf_indices[k]])
                                   % len(recipes))
-            index = recipes_string.find(recipe_scores)
-            if index != -1:
-                break
-        print(index)
     else:
         print("Usage:  " + sys.argv[0] + " <recipe-count>")
